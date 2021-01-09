3d66a82b7ad026f67bf4dade818747f507b2b459
Dane <nevesdane@gmail.com>
2021-01-08 21:11:02 -0800

    updated home slogan. Added concentra solver to projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadingStrategy, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
